c6415b5aa24388c43847fbd3274498415b2832a1
DongkwanYi <141334530+DongkwanYi@users.noreply.github.com>
2023-08-07 11:04:51 +0900

    Update lcd_driver_spi.cpp

--- lcd_driver_spi.cpp
+++ lcd_driver_spi.cpp
@@ -144,12 +144,12 @@ void lcdStart()
 	//     0xA0 => Column Address 0 Mapped to SEG0
 	//     0xA1 => Column Address 0 Mapped to SEG127
 
-  lcdWriteCommand(0xc8);	// set com scan direction
+	lcdWriteCommand(0xc8);	// set com scan direction
 	//     0xC0 => Scan from COM0 to 63
 	//     0xC8 => Scan from COM63 to 0
 
-#else // #if defined(LCD_VERTICAL_INVERT)
-  lcdWriteCommand(0xA0);	// set segment remap
+#else //	#if defined(LCD_HORIZONTAL_INVERT)
+	lcdWriteCommand(0xA0);	// set segment remap
 	//     0xA0 => Column Address 0 Mapped to SEG0
 	//     0xA1 => Column Address 0 Mapped to SEG127
 
